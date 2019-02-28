afdebcbe94f98da66f28a2f6a10c808c3808168e
John M. Lindemon <jlindemon@gmail.com>
2019-02-27 21:23:57 -0500

    Temporary

---
+++ test cases/cuda/5 threads/lib.cpp
@@ -0,0 +1,7 @@
+#include "lib.hpp"
+
+Library::Library() {}
+
+Library::~Library() {}
+
+int Library::get() { return 1; }
---
+++ test cases/cuda/5 threads/lib.hpp
@@ -0,0 +1,15 @@
+// lib.hpp
+
+#ifndef LIB_H
+#define LIB_H
+
+class Library
+{
+public:
+   Library();
+   virtual ~Library();
+   int get();
+};
+
+
+#endif /* LIB_H */
